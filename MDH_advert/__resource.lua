bad27d35e7d80a3cc8752ce1a2c0d1e3b5dd1bee
Harrison M <hmayotte@main.mdhholdings.org>
2020-04-27 09:06:30 -0400

    Update

--- MDH_advert/__resource.lua
+++ MDH_advert/__resource.lua
@@ -1,3 +1,2 @@
-description 'MDH Holdings - Advertizement Script'
 client_script 'cl_phone.lua'
 server_script 'sv_phone.lua'
--- MDH_advert/sv_phone.lua
+++ MDH_advert/sv_phone.lua
@@ -1,3 +1,39 @@
--- Basic Advert Script made by MDH Holdings
+RegisterServerEvent("chatMessage")
+AddEventHandler('chatMessage', function(source, n, message)
+	pm = stringsplit(message, " ")
+	if pm[1] == "/ad" then
+		CancelEvent()
+		--table.remove(pm, 1)
+		if pm[2] == nil then
+			TriggerClientEvent("chatMessage", source, "", {0,0,0}, "^1Usage: ^7/ad [company] ^3 Companies: fleeca, maze, ammunation, lscustoms, pegasus, taxi, traffic")
+		elseif pm[2] ~= nil then
+			TriggerClientEvent("mdh:ad:composeAd", source, pm[2])
+		end
+	elseif pm[1] == "/toggleads" then
+			TriggerClientEvent("mdh:ad:toggle", source)
+			RconPrint("Toggle Ads")	
+	end
+end)
 
-RegisterServerEvent("chatMessage")AddEventHandler('chatMessage',function(a,b,c)pm=stringsplit(c," ")if pm[1]=="/ad"then CancelEvent()if pm[2]==nil then TriggerClientEvent("chatMessage",a,"",{0,0,0},"^1Usage: ^7/ad [company] ^3 Companies: fleeca, maze, ammunation, lscustoms, pegasus, taxi, traffic")elseif pm[2]~=nil then TriggerClientEvent("mdh:ad:composeAd",a,pm[2])end elseif pm[1]=="/toggleads"then TriggerClientEvent("mdh:ad:toggle",a)RconPrint("Toggle Ads")end end)RegisterServerEvent("mdh:ad:pushAd")AddEventHandler("mdh:ad:pushAd",function(d,c)TriggerClientEvent("mdh:ad",-1,d,c)end)function stringsplit(e,f)if f==nil then f="%s"end;local g={}i=1;for h in string.gmatch(e,"([^"..f.."]+)")do g[i]=h;i=i+1 end;return g end;function tablelength(j)local k=0;for l in pairs(j)do k=k+1 end;return k end
+RegisterServerEvent("mdh:ad:pushAd")
+AddEventHandler("mdh:ad:pushAd", function(company, message)
+	TriggerClientEvent("mdh:ad", -1, company, message)
+end)
+
+function stringsplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
+
+function tablelength(T)
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
+end
